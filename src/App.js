30b3f3dea79107427b18c58c1831da4b3f4e1996
fundfusion <jiitinsights2023@gmail.com>
2024-07-11 17:33:10 +0530

    Final commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SortingComponent from './Components/Visualiser/sortingComponent';
+
+function App() {
+	return (
+		<div className="App">
+			<SortingComponent />
+		</div>
+	);
+}
+
+export default App;
